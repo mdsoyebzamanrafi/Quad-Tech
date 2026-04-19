5ab1b131561b1caa0f537812b824eb127bdae900
AbrarUI12 <135985867+AbrarUI12@users.noreply.github.com>
2026-04-19 23:51:21 +0600

    Remove duplicate Quad-Tech folder

--- Quad-Tech/backend/routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { processPayment, getPaymentConfig } from '../controllers/paymentController.js';
-import { protect } from '../middleware/authMiddleware.js';
-
-router.route('/process').post(protect, processPayment);
-router.route('/config').get(protect, getPaymentConfig);
-
-export default router;
